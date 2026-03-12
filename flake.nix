d326a44def6ad4942868e77bda1bce1197c290f9
jazzkid <jazzkid@jazzkid.xyz>
2026-03-12 18:25:41 +0000

    app: build-all

--- flake.nix
+++ flake.nix
@@ -13,10 +13,23 @@
     };
   };
   outputs = { self, nixpkgs, agenix, opencode, ... }:
+  let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+    lib = pkgs.lib;
+  in
   {
     packages.x86_64-linux = {
       opencode = opencode.packages.x86_64-linux.default;
       agenix = agenix.packages.x86_64-linux.default;
+    };
+
+    apps.x86_64-linux = {
+      build-all = {
+        type = "app";
+        program = (pkgs.writeShellScriptBin "build-all" ''
+          ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: _: "nix build --no-link --print-out-paths .#${name}") self.packages.x86_64-linux)}
+        '').outPath + "/bin/build-all";
+      };
     };
   };
 }
